9a5c5afbd5b13dd64cf1b11086218cbc332345a9
Wissididom <30803034+Wissididom@users.noreply.github.com>
2023-01-18 10:47:43 +0100

    Create deps.ts

---
+++ deps.ts
@@ -0,0 +1,4 @@
+export * as DiscordJS from "npm:discord.js@^14.7.1";
+export * as DotEnv from "npm:dotenv@^16.0.3";
+export * as pg from "npm:pg@^8.7.1";
+export * as sequelize from "npm:sequelize@^6.19.0";
