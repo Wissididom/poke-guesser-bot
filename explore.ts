69d9ec22a95e21a0e934defc5e7b87e13bc9a8d0
Wissididom <30803034+Wissididom@users.noreply.github.com>
2023-01-18 11:21:23 +0100

    Import from deps.ts in explore.ts

--- explore.ts
+++ explore.ts
@@ -1,7 +1,9 @@
-import { ActionRowBuilder, ButtonBuilder, ButtonStyle, ChatInputCommandInteraction, GuildMember, EmbedBuilder, AttachmentBuilder, SlashCommandBuilder } from "npm:discord.js";
+import { DiscordJS } from "./deps.ts";
 import Database from "./data/postgres.ts";
 import Language from "./language.ts";
 import Util from "./util.ts";
+
+const { ActionRowBuilder, ButtonBuilder, ButtonStyle, ChatInputCommandInteraction, GuildMember, EmbedBuilder, AttachmentBuilder, SlashCommandBuilder } = DiscordJS;
 
 export default class Explore {
 
