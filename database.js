a10e2196920c0c6a3823770f73cb0c82d356bda3
Wissididom <30803034+Wissididom@users.noreply.github.com>
2024-10-24 00:06:31 +0200

    Recreate database class without replit connection

--- database.js
+++ database.js
@@ -1,17 +1,115 @@
-const Database = require("@replit/database");
+class Database {
+  #db = null;
 
-const originalGet = Database.prototype.get;
+  constructor() {}
 
-async function get(key, options) {
-  return await originalGet.call(this, key, options).then((res) => {
-    if (res.ok) {
-      return res.value;
+  async initDb() {
+    this.#db = await new Promise((resolve, reject) => {
+      let db = new sqlite3.Database("./database.db", (err) => {
+        if (err) {
+          console.error(err.message);
+          reject(err);
+          return;
+        }
+        console.log("Successfully connected to SQLite");
+        resolve(db);
+      });
+    });
+  }
+
+  async get(key) {
+    let rows = await new Promise((resolve, reject) => {
+      db.all(`SELECT * FROM kv WHERE key = ?`, [key], (err, rows) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+        resolve(rows);
+      });
+    });
+    if (rows.length > 0) {
+      return rows[0].value;
     } else {
-      return res;
+      return "";
     }
-  });
-}
+  }
 
-Database.prototype.get = get;
+  async set(key, value) {
+    await new Promise((resolve, reject) => {
+      db.run(
+        `CREATE TABLE IF NOT EXISTS kv (key TEXT PRIMARY KEY, value TEXT)`,
+        [],
+        (err) => {
+          if (err) {
+            console.error(err.message);
+            reject(err);
+            return;
+          }
+          console.log(`Successfully created table 'kv'`);
+          resolve();
+        },
+      );
+    });
+    await new Promise((resolve, reject) => {
+      db.run(
+        `INSERT INTO kv (key, value) VALUES (?, ?) ON CONFLICT(key) DO UPDATE SET value = excluded.value`,
+        [key, value],
+        (err) => {
+          if (err) {
+            console.error(err.message);
+            reject(err);
+            return;
+          }
+          console.log(
+            `Successfully upserted value '${keys[i]}'='${req.body[keys[i]]}'`,
+          );
+          resolve();
+        },
+      );
+    });
+  }
+
+  async delete(key) {
+    let rows = await new Promise((resolve, reject) => {
+      db.all(`SELECT * FROM kv WHERE key = ?`, [key], (err, rows) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+        resolve(rows);
+      });
+    });
+    if (rows.length > 0) {
+      await new Promise((resolve, reject) => {
+        db.run(`DELETE FROM kv WHERE key = ?`, [key], (err) => {
+          if (err) {
+            console.error(err.message);
+            reject(err);
+            return;
+          }
+          console.log(`Successfully deleted '${key}'`);
+          resolve();
+        });
+      });
+    }
+  }
+
+  async list() {
+    let rows = await new Promise((resolve, reject) => {
+      db.all(`SELECT * FROM kv`, [], (err, rows) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+        resolve(rows);
+      });
+    });
+    if (rows.length > 0) {
+      return rows.map((row) => row.key).join("\n");
+    } else {
+      return "";
+    }
+  }
+}
 
 module.exports = Database;
